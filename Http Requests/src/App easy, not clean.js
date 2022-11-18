c8cc5b76c6566ef8f219b6ad20b568a953a2ab2d
Vasilena Tsacheva <vasilenatsacheva@gmail.com>
2022-11-18 14:43:55 +0200

    Http Requests Project

---
+++ Http Requests/src/App easy, not clean.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	async function fetchMovieHandler() {
+		setIsLoading(true);
+		setError(null);
+
+		try {
+			const response = await fetch('https://swapi.dev/api/films');
+			if (!response.ok) {
+				throw new Error('Something went wrong!');
+			}
+
+			const data = await response.json();
+
+			const transformedMovies = data.results.map((movieData) => {
+				return {
+					id: movieData.episode_id,
+					title: movieData.title,
+					openingText: movieData.opening_crawl,
+					releaseDate: movieData.release_date,
+				};
+			});
+
+			setMovies(transformedMovies);
+		} catch (error) {
+			setError(error.message);
+		}
+		setIsLoading(false);
+	}
+
+	return (
+		<React.Fragment>
+			<section>
+				<button onClick={fetchMovieHandler}>Fetch Movies</button>
+			</section>
+			<section>
+				{!isLoading && <MoviesList movies={movies} />}
+				{!isLoading && movies.length === 0 && !error && <p>No movies found.</p>}
+				{isLoading && <p>Loading...</p>}
+				{!isLoading && error && <p>{error}</p>}
+			</section>
+		</React.Fragment>
+	);
+}
+
+export default App;
