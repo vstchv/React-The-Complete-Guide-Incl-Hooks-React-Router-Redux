b902d0d8824bc5ca3c97e47000a7e788829d812a
Vasilena Tsacheva <vasilenatsacheva@gmail.com>
2022-12-05 17:11:57 +0200

    Adding cart context

--- Food Order App/src/components/Layout/HeaderCartButton.js
+++ Food Order App/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,21 @@
-import React from 'react';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
+import { useContext } from 'react';
 
 const HeaderCartButton = (props) => {
+	const cartCtx = useContext(CartContext);
+	const numberOfCartItems = cartCtx.items.reduce((current, item) => {
+		return current + item.amount;
+	}, 0);
+
 	return (
 		<button className={styles.button} onClick={props.onCartClick}>
 			<span className={styles.icon}>
 				<CartIcon />
 			</span>
 			<span>Your Cart</span>
-			<span className={styles.badge}>3</span>
+			<span className={styles.badge}>{numberOfCartItems}</span>
 		</button>
 	);
 };
